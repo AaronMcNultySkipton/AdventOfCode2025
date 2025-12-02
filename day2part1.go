0626dc705959ef87dcd0d2ad28b12864928c678d
Aaron McNulty <aaron.mcnulty@skipton.co.uk>
2025-12-02 11:57:14 +0000

    Day 2 Part 1

---
+++ day2part1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+func d2p1() {
+	content := readFile("day2Input.txt")
+
+	total := 0
+
+	for _, line := range content {
+		log.Println(line)
+
+		Ranges := strings.Split(line, ",")
+
+		log.Println(Ranges)
+
+		for _, r := range Ranges {
+			Bounds := strings.Split(r, "-")
+			start, _ := strconv.Atoi(Bounds[0])
+			end, _ := strconv.Atoi(Bounds[1])
+			for i := start; i <= end; i++ {
+				j := strconv.Itoa(i)
+				if len(j)%2 == 0 {
+					firstHalf := j[0 : len(j)/2]
+					secondHalf := j[len(j)/2:]
+					if firstHalf == secondHalf {
+						log.Printf("Found matching number: %v", j)
+						total += i
+					}
+				}
+			}
+		}
+
+	}
+
+	log.Printf("Total sum of matching numbers: %v", total)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
-	d1p1()
-	d1p2()
+	//d1p1()
+	//d1p2()
+	d2p1()
 }
